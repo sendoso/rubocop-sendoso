63eb15e1f045b9d6be5c3e40f020b858d8a37ba7
Marc Fernandez <marc.fernandezg@gmail.com>
2021-06-14 17:52:37 -0700

    Initial commit.

---
+++ lib/sendoso/rubocop.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rubocop/version"
+
+module Sendoso
+  module Rubocop
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
